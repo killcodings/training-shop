16e57cde1aaed9f9e4cf7add05d16991f996c515
killcodings <kontaktid@ya.ru>
2022-02-17 04:35:41 +0300

    add HashRouter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import "./App.css";
 import Header from "./components/header/header";
 import MainPage from "./components/mainPage/mainPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Men from "./components/men/men";
 import Women from "./components/women's/women's";
 
 function App() {
   return (
     <div className="site">
-      <BrowserRouter>
+      <HashRouter>
         <Header />
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/men" element={<Men />} />
           <Route path="/women" element={<Women />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
